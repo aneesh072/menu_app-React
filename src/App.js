d4d8e27551f45fd22176fe60fee651dc61644c98
aneesh072 <aneeshkarki292@gmail.com>
2022-08-05 19:11:03 +1000

    adding menu images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import items from '../src/components/data.js';
+import Menu from './components/Menu';
+import Catagories from './components/Catagories';
 
-const App = () => {};
+const App = () => {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categoeies, setCategoeies] = useState([]);
+
+  return (
+    <main>
+      <section className="menu-section"></section>
+      <div className="title">
+        <h2>Our Menu</h2>
+        <div className="underline"> </div>
+      </div>
+      <Catagories />
+      <Menu items={menuItems} />
+    </main>
+  );
+};
 
 export default App;
