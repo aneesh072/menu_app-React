f9358d13e7d247373aca5fdd2d667626ca612bce
aneesh072 <aneeshkarki292@gmail.com>
2022-08-05 19:34:32 +1000

    manual data filterning

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ const App = () => {
   const [menuItems, setMenuItems] = useState(items);
   const [categoeies, setCategoeies] = useState([]);
 
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
   return (
     <main>
       <section className="menu-section"></section>
@@ -15,7 +24,7 @@ const App = () => {
         <h2>Our Menu</h2>
         <div className="underline"> </div>
       </div>
-      <Catagories />
+      <Catagories filterItems={filterItems} />
       <Menu items={menuItems} />
     </main>
   );
--- src/components/Catagories.js
+++ src/components/Catagories.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const Catagories = () => {
-  return <div>Catagories</div>;
+const Catagories = ({ filterItems }) => {
+  return (
+    <div className="btn-container">
+      <button className="filter-btn" onClick={() => filterItems('all')}>
+        All
+      </button>
+      <button className="filter-btn" onClick={() => filterItems('breakfast')}>
+        Breakfast
+      </button>
+    </div>
+  );
 };
 
 export default Catagories;
